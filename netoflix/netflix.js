836eef2de5310e0ad01fc11680079aa0e3835acb
ishisharma26 <ishas2390@gmail.com>
2024-08-31 20:59:19 +0530

    Frontend projects changes

---
+++ netoflix/netflix.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#darkm1").click(function () {     
+      $("body").css({
+        backgroundColor: "white",
+      });
+      $(".enjoy").css({
+        backgroundColor: "white",
+        color: "black",
+      });
+      $(".enjoy1").css({
+        color: "black",
+      });
+      $(".download").css({
+        backgroundColor: "white",
+        color: "black",
+      });
+      $(".look").css({
+        backgroundColor: "white",
+        color: "black",
+      });
+      $(".faq").css({
+        backgroundColor: "white",
+        color: "black",
+      });
+      $(".footer").css({
+        backgroundColor: "white",
+        color: "black",
+      });
+      $("a").css({
+        color: "black",
+      });
+    });
+  });
